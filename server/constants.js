c3759552cdf6391a6debdf20cd2564286ee0ba14
Ishaan Verma <ishaanverma97@gmail.com>
2022-01-16 23:44:56 -0800

    Inital setup for express app

---
+++ server/constants.js
@@ -0,0 +1,11 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/server';
+
+module.exports = {
+  MONGO_URL,
+  PORT,
+}
---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const { PORT, MONGO_URL } = require('./constants');
+
+const app = express();
+
+// setup middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
+
+// setup basic routes
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+// connect to database
+mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.on('open', () => {
+  console.log('Connected to DB');
+})
+
+// start listening
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
